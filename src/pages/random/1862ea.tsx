df0fd9e432fae8d9a7591b7eb4d0a3192e3479c1
Gregor MacDougall <gmacdougall0+github@gmail.com>
2019-02-16 16:04:58 -0500

    Add randomizer for 1862EA
    1862: Railway Mania in Eastern Counties needs randomization for the 16
    available companies and their assigned privates.

---
+++ src/pages/random/1862ea.tsx
@@ -0,0 +1,61 @@
+import shuffle from 'lodash.shuffle';
+import React from 'react';
+
+import Layout from '../../components/layout';
+import OrderedList from '../../components/ordered_list';
+
+const companies: ReadonlyArray<string> = shuffle([
+  "E&H - Ely & Huntingdon",
+  "ECR - Eastern Counties Railway",
+  "ENR - East Norfolk Railway",
+  "ESR - East Suffolk Railway",
+  "EUR - Eastern Union Railway",
+  "FDR - Felixstowe Dock & Railway",
+  "I&B - Ipswich & Bury Railway Black/Grey Bury",
+  "L&D - Lynn & Dereham Railway",
+  "L&E - Lynn & Ely Railway Red/Yellow",
+  "L&H - Lynn & Hunstanton Railway Blue/Red",
+  "N&B - Norwich & Brandon Railway",
+  "N&E - Northern & Eastern Railway Grey/Green",
+  "N&S - Norfolk & Suffolk, Joint Railway",
+  "Y&N - Yarmouth & Norwich Railway Black/Red",
+  "NGC - Newmarket and Great Chesterford Railway",
+  "SVR - Colchester, Stour Valley, Sudbury & Halstead",
+  "WNR - West Norfolk Railway",
+  "W&F - Wells & Fakenham Railway",
+  "WVR - Waveney Valley Railway",
+  "WStI - Wisbech, St Ives & Cambridge Junction Rwy"
+]).slice(4);
+
+const permits: ReadonlyArray<string> = shuffle([
+  'Freight',
+  'Freight',
+  'Freight',
+  'Freight',
+  'Freight',
+  'Freight',
+  'Express',
+  'Express',
+  'Express',
+  'Express',
+  'Express',
+  'Local',
+  'Local',
+  'Local',
+  'Local',
+  'Local',
+]);
+
+const pairs: ReadonlyArray<string> = companies.map((c, i) => (
+  `${c} (${permits[i]})`
+));
+
+const Page: React.SFC = () => (
+  <Layout>
+    <h1>1822+</h1>
+
+    <OrderedList heading='Companies' list={pairs} />
+  </Layout>
+);
+
+export default Page;
