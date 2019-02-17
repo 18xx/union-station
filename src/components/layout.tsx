ef32828ec2279fde6efe89a0d91ce2dcf10e13e8
Gregor MacDougall <gmacdougall0+github@gmail.com>
2019-02-17 08:24:00 -0500

    Typescript-ify layout

---
+++ src/components/layout.tsx
@@ -0,0 +1,45 @@
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+
+import Header from './header';
+import './layout.css';
+
+interface LayoutProps {
+  readonly children: ReadonlyArray<JSX.Element>;
+}
+
+const renderFn: (
+  children: ReadonlyArray<JSX.Element>
+) => (data: any) => JSX.Element = children => data => (
+  <>
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `0px 1.0875rem 1.45rem`,
+        paddingTop: 0,
+      }}
+    >
+      <main>{children}</main>
+      <footer />
+    </div>
+  </>
+);
+
+const Layout: React.SFC<LayoutProps> = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={renderFn(children)}
+  />
+);
+
+export default Layout;
