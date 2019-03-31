7ab1963ca3960f9bd6907b22c147fd3aa9f31a92
Gregor MacDougall <gmacdougall0+github@gmail.com>
2019-03-31 08:21:23 -0400

    Add 18NW
    This adds 18NW to the randomizer list.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ const IndexPage: React.SFC = () => (
       <li><Link to="/random/1822ca/">1822CA</Link></li>
       <li><Link to="/random/1846/">1846</Link></li>
       <li><Link to="/random/1862ea/">1862: Railway Mania in Eastern Counties</Link></li>
+      <li><Link to="/random/18nw/">18NW: Railroading in the "Great Northwest"</Link></li>
     </ul>
   </Layout>
 );
---
+++ src/pages/random/18nw.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Layout from '../../components/layout';
+import OrderedList from '../../components/ordered_list';
+
+import sampleSize from 'lodash.samplesize';
+
+const privates: ReadonlyArray<string> = [
+  'P1 - IRNC',
+  'P2 - Mine',
+  'P4 - Ferry',
+  'P6 - Oil',
+].concat(sampleSize([
+  'P3a - Timber',
+  'P3b - Steamboat',
+  'P3c - Steamship',
+], 1)).concat(sampleSize([
+  'P5a - Port',
+  'P5b - Shovel',
+  'P5c - Tunnel',
+], 1)).concat(sampleSize([
+  'P7a - Kettle Valley',
+  'P7b - Camas Prairie',
+  'P7c - Kaslo & Slocan',
+  'P7d - Tacoma & Eastern',
+  'P7e - Oregon Short Line',
+], 3)).sort();
+
+const Page: React.SFC = () => {
+  return (
+    <Layout>
+      <h1>18NW: Railroading in the "Great Northwest"</h1>
+
+      <OrderedList heading="Private Companies" list={privates} />
+    </Layout>
+  );
+}
+
+export default Page;
