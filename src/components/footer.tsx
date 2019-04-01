38476aa8ea241d7676fdc32c98cbcc8b7c4b1c20
Gregor MacDougall <gmacdougall0+github@gmail.com>
2019-04-01 16:03:13 -0400

    Add in a not so fancy footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer: React.SFC = () => (
+  <footer style={{
+    backgroundColor: '#ddd',
+    padding: '0.5rem',
+    textAlign: 'center',
+  }}>
+    <a href="https://github.com/18xx/union-station">GitHub Source</a>
+  </footer>
+);
+
+export default Footer;
+
