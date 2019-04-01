9bd7733626b93c1112a00153ece18e03b5c5909f
Gregor MacDougall <gmacdougall0+github@gmail.com>
2019-04-01 11:12:30 -0400

    Randomize in function
    This ensures a fresh random generation on every visit to the page.

--- src/pages/random/18nw.tsx
+++ src/pages/random/18nw.tsx
@@ -5,28 +5,28 @@ import OrderedList from '../../components/ordered_list';
 
 import sampleSize from 'lodash.samplesize';
 
-const privates: readonly string[] = [
-  'P1 - IRNC',
-  'P2 - Mine',
-  'P4 - Ferry',
-  'P6 - Oil',
-].concat(sampleSize([
-  'P3a - Timber',
-  'P3b - Steamboat',
-  'P3c - Steamship',
-], 1), sampleSize([
-  'P5a - Port',
-  'P5b - Shovel',
-  'P5c - Tunnel',
-], 1), sampleSize([
-  'P7a - Kettle Valley',
-  'P7b - Camas Prairie',
-  'P7c - Kaslo & Slocan',
-  'P7d - Tacoma & Eastern',
-  'P7e - Oregon Short Line',
-], 3)).sort();
-
 const Page: React.SFC = () => {
+  const privates: readonly string[] = [
+    'P1 - IRNC',
+    'P2 - Mine',
+    'P4 - Ferry',
+    'P6 - Oil',
+  ].concat(sampleSize([
+    'P3a - Timber',
+    'P3b - Steamboat',
+    'P3c - Steamship',
+  ], 1), sampleSize([
+    'P5a - Port',
+    'P5b - Shovel',
+    'P5c - Tunnel',
+  ], 1), sampleSize([
+    'P7a - Kettle Valley',
+    'P7b - Camas Prairie',
+    'P7c - Kaslo & Slocan',
+    'P7d - Tacoma & Eastern',
+    'P7e - Oregon Short Line',
+  ], 3)).sort();
+
   return (
     <Layout>
       <h1>18NW: Railroading in the "Great Northwest"</h1>
