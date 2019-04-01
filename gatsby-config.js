d1f59762bcaaff8a9807b460830fa24997e8e9c0
Gregor MacDougall <gmacdougall0+github@gmail.com>
2019-04-01 11:01:33 -0400

    Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-typescript`,
+    `gatsby-plugin-favicon`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
